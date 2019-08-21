bab7aed4b3720c570de739675857562e85ca76a3
hosoolee <hosoo.lee@atos.net>
2019-08-21 04:05:45 +0200

    Updates

---
+++ precache-manifest.28ec9ac407f9acd129867a1d56540651.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b2480bc8f3cfce76fc6",
+    "url": "/bepro/static/css/main.a71b65d4.chunk.css"
+  },
+  {
+    "revision": "1b2480bc8f3cfce76fc6",
+    "url": "/bepro/static/js/main.3e3523c5.chunk.js"
+  },
+  {
+    "revision": "2d47a81be545bf17660b",
+    "url": "/bepro/static/js/runtime~main.b11f138e.js"
+  },
+  {
+    "revision": "e02a27134a6b39a7b9a5",
+    "url": "/bepro/static/js/2.9ded8569.chunk.js"
+  },
+  {
+    "revision": "bf3ca4ed2b938d14ecc7b5e2a75e0df5",
+    "url": "/bepro/static/media/team-4-800x800.bf3ca4ed.jpg"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/bepro/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/bepro/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/bepro/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/bepro/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/bepro/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/bepro/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/bepro/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/bepro/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/bepro/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/bepro/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/bepro/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/bepro/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/bepro/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/bepro/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/bepro/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/bepro/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/bepro/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/bepro/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/bepro/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/bepro/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/bepro/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/bepro/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/bepro/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "9094dc527e84e46de205a4a2d5027055",
+    "url": "/bepro/static/media/profile-cover.9094dc52.jpg"
+  },
+  {
+    "revision": "fcf40d6a94df7a5a340cc3d9e9f1aca1",
+    "url": "/bepro/static/media/scss.fcf40d6a.png"
+  },
+  {
+    "revision": "368685db949cddcd901ba54559a727d9",
+    "url": "/bepro/static/media/angular.368685db.jpg"
+  },
+  {
+    "revision": "c4eb3d0be0cf5a2d156a123bdf7fb40d",
+    "url": "/bepro/static/media/react.c4eb3d0b.jpg"
+  },
+  {
+    "revision": "232d5761f9b988946a26ca83ab7d55a9",
+    "url": "/bepro/static/media/test.232d5761.png"
+  },
+  {
+    "revision": "ce31637b0e4455325bc14a7f85eeb7b6",
+    "url": "/bepro/static/media/node.ce31637b.png"
+  },
+  {
+    "revision": "275a2629582aa2af3469364c6748cf38",
+    "url": "/bepro/static/media/argon-react-white.275a2629.png"
+  },
+  {
+    "revision": "911bf501df31cd383d8515b4577fe5f3",
+    "url": "/bepro/index.html"
+  }
+];
